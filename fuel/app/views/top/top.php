3cc5c4e64a1c849c1306a0ed908d5638fb031838
fuchi15 <sakuya815@gmail.com>
2016-01-10 16:12:27 +0900

    売上情報追加

--- fuel/app/views/top/top.php
+++ fuel/app/views/top/top.php
@@ -8,9 +8,9 @@
   <body>
     <div class="content">
       <div class="frame"><p><?php echo Html::anchor('index.php/members/infosearch','会員検索'); ?></p></div>
-      <div class="frame"><p><?php echo Html::anchor('index.php/item/item_search','商品検索')?></p></div>
+      <div class="frame"><p><?php echo Html::anchor('index.php/item/search','商品検索')?></p></div>
       <div class="frame"><p><?php echo Html::anchor('index.php/order/order','ピザ注文') ?></p></div>
-      <div class="frame"><p><?php echo Html::anchor('index.php/item/item_registration','商品登録'?>'</p></div>
+      <div class="frame"><p><?php echo Html::anchor('index.php/item/registration','商品登録')?></p></div>
       <div class="frame"><p>売上情報</p></div>
     </div>
   </body>
