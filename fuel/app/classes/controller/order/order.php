a5e5ce0741f558926dcae05d49680d8be9df237d
44x1carbon <b4079@oic.jp>
2016-01-10 01:45:32 -0800

    a

--- fuel/app/classes/controller/order/order.php
+++ fuel/app/classes/controller/order/order.php
@@ -5,7 +5,6 @@ class Controller_Order_Order extends Controller
     const ORDER = 'order';
     public function action_index()
     {
-        
         $data['item_list'] = Model_Item::find('all');
         return View::forge("order/order",$data);
     }
