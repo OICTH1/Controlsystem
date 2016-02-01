25acc14e09da5346be761a90c1cafef13aa31780
やまごん <yamagon@yamagon-no-MacBook-Pro.local>
2016-02-01 09:39:38 +0900

    注文画面レイアウト変更中

--- fuel/app/views/order/order.php
+++ fuel/app/views/order/order.php
@@ -13,29 +13,65 @@
 		<div class="back">
 			<p><?php echo Html::anchor('index.php/top','戻る') ?></p>
 		</div>
+		<div style="clear :both;"></div>
 	</div>
 	<div class="content">
-		<div class="content_left">
-			<div class="content_left_top">
-				<div class="item_search">
-					<from>
-						<p><b>商品検索</b></p>
-						<p>
-						<input type="radio"  name="category" value="pizza"><label for="pizza">ピザ</label>
-						<input type="radio"  name="category" value="drink"><label for="drank">ドリンク</label>
-						<input type="radio"  name="category" value="side"><label for="side">サイド</label>
-						</p>
-					</form>
-				</div>
-			</div>
-			<div class="content_left_bottom">
-				<div class="item_view">
-					<table border="1">
-						<thead class="view_table_head">
+		<div class="content_top">
+			<div class="content_top_top">
+					<table class="order_table"  border="1">
+						<thead class="order_table_head">
 							<tr>
-								<th class="view">商品名</th>
+								<th id="number">No.</th>
+								<th id="item_name">商品名</th>
+								<th>個数</th>
+								<th>単価</th>
+								<th>小計</th>
 							</tr>
 						</thead>
+						<tbody class="order_table_scroll">
+						</tbody>
+					</table>
+			</div>
+			<div class="content_top_bottom">
+					<table class="sum_table" border="1">
+						<tbody class="sum_table_body">
+							<tr>
+								<th>数量</th>
+								<td></td>
+							</tr>
+							<tr>
+								<th>合計金額</th>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+			</div>
+		</div>
+		<div class="content_center">
+			<div class="item_title">ピザ
+			</div>
+			<div class="item_title">サイド
+			</div>
+			<div class="item_title">ドリンク
+			</div>
+			<div style="clear :both;"></div>
+		</div>
+		<div class="content_bottom">
+			<div class="content_bottom_left">
+				<a class="indexlist" id='a'>あ</a></br>
+				<a class="indexlist" id='k'>か</a></br>
+				<a class="indexlist" id='s'>さ</a></br>
+				<a class="indexlist" id='t'>た</a></br>
+				<a class="indexlist" id='n'>な</a></br>
+				<a class="indexlist" id='h'>は</a></br>
+				<a class="indexlist" id='m'>ま</a></br>
+				<a class="indexlist" id='y'>や</a></br>
+				<a class="indexlist" id='r'>ら</a></br>
+				<a class="indexlist" id='w'>わ</a>
+
+			</div>
+			<div class="content_bottom_center">
+					<table border="1">
 						<tbody class="view_table_scroll">
 							<?php foreach($item_list as $item){
 									$class = "item ";
@@ -48,60 +84,41 @@
 							}?>
 						</tbody>
 					</table>
+			</div>
+			<div class="content_bottom_right">
+				<div class="content_bottom_right_top">
+					<div class="numbox">
+						<div class="numboxrow">
+							<div class="numbutton" id='7'>7</div>
+							<div class="numbutton" id='8'>8</div>
+							<div class="numbutton" id='9'>9</div>
+						</div>
+						<div class="numboxrow">
+							<div class="numbutton" id='4'>4</div>
+							<div class="numbutton" id='5'>5</div>
+							<div class="numbutton" id='6'>6</div>
+						</div>
+						<div class="numboxeow">
+							<div class="numbutton" id='1'>1</div>
+							<div class="numbutton" id='2'>2</div>
+							<div class="numbutton" id='3'>3</div>
+						</div>
+						<div class="numbox">
+							<div class="numbutton" id='Delete'>消</div>
+							<div class="numbutton" id='0'>0</div>
+							<div class="numbutton" id='decision'>決</div>
+						</div>
+						<div style="clear:both;"></div>
+					</div>
+				</div>
+				<div class="content_bottom_right_bottom">
+					<div class="allclear">
+					</div>
+					<div class="commit">
+					</div>
 				</div>
 			</div>
 		</div>
-		<div class="content_right">
-			<div class="content_right_top">
-				<div class="item_add">
-					<p>商品名<span class="slct-item-name"></span>
-					<from>
-						<p>サイズ
-							<span>
-								<input type="radio" id="s" name="size" value="s" checked><label for="s">S</label>
-								<input type="radio" id="m" name="size" value="m"><label for="m">M</label>
-								<input type="radio" id="l" name="size" value="l"><label for="l">L</label>
-							</span>
-						</p>
-						<p>数量
-							<span>
-								<input type="radio" id='num-1' name="num" value="1" checked><label for="num-1">1</label>
-								<input type="radio" id='num-2' name="num" value="2"><label for="num-2">2</label>
-								<input type="radio" id='num-3' name="num" value="3"><label for="num-3">3</label>
-								<input type="radio" id='num-4' name="num" value="4"><label for="num-4">4</label>
-								<input type="radio" id='num-5' name="num" value="5"><label for="num-5">5</label>
-							</span>
-						</p>
-						<input type="button" value="カートに追加" class="cartin-btn">
-					</form>
-				</div>
-			</div>
-			<div class="content_right_center">
-				<div class="cart">
-					<p>カート</P>
-						<table class="order_table"  border="1">
-							<thead class="order_table_head">
-								<tr>
-									<th id="number">&nbsp;</th>
-									<th id="item_name">商品名</th>
-									<th>個数</th>
-									<th>サイズ</th>
-									<th>&nbsp;&nbsp;</th>
-								</tr>
-							</thead>
-							<tbody class="order_table_scroll">
-
-							</tbody>
-						</table>
-				</div>
-			</div>
-			<div class="content_right_bottom">
-				<?php echo Form::Open('index.php/order/cfm'); ?>
-				<?php echo Form::Button('button','確認'); ?>
-				<?php echo Form::Close();?>
-			</div>
-		</div>
-	</div>
 	<div id="msg"></div>
 	<?php echo Asset::js('jquery-1.11.3.min.js')?>
 	<script type="text/javascript">
