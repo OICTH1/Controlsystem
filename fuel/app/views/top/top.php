732c4cdc1d7bb495e7f647cd7baa1ecb5ae79ef3
fuchi15 <sakuya815@gmail.com>
2016-02-03 00:39:31 +0900

    商品登録　登録機能完了

--- fuel/app/views/top/top.php
+++ fuel/app/views/top/top.php
@@ -9,10 +9,9 @@
     <div class="content">
       <div class="frame"><p><?php echo Html::anchor('index.php/members/infosearch','会員検索'); ?></p></div>
       <div class="frame"><p><?php echo Html::anchor('index.php/order/order','注文登録') ?></p></div>
-      <!--<div class="frame"><p><?php echo Html::anchor('index.php/item/search','商品検索')?></p></div>
-      <div class="frame"><p><?php echo Html::anchor('index.php/item/registration','商品登録')?></p></div>-->
+      <div class="frame"><p><?php echo Html::anchor('index.php/item/search','商品検索')?></p></div>
+      <div class="frame"><p><?php echo Html::anchor('index.php/item/registration','商品登録')?></p></div>
       <div class="frame"><p><?php echo Html::anchor('index.php/earnings/earnings','売上情報')?></p></div>
-      <?php echo $_SERVER['SERVER_ADDR'];?>
     </div>
   </body>
 </html>
