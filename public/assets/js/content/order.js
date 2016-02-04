2aabff1b6f1b8277f0862e6b5188b7dfd5ed4697
やまごん <yamagon@yamagon-no-MacBook-Pro.local>
2016-02-04 21:34:57 +0900

    二列の表示を正しく

--- public/assets/js/content/order.js
+++ public/assets/js/content/order.js
@@ -48,109 +48,29 @@ function listUpdata(data){
       text += "<div class='viewlistrow'>";
     }
 
-    if(name=="pizza"){
-      text += "<div class='viewitem' id='" + data[i].size + "' place='" + data[i].place + "'>" + data[i].name + "(" + data[i].size +")</div>";
+    if(category == "pizza"){
+      text += "<div class='viewitem' >" + data[i].name + " (" + data[i].size +") <input type='hidden' name='" + data[i].size + "' value='" + data[i].place + "'></div>";
     }else{
-      text += "<div class='viewitem' name=''>" + data[i].name + "" + data[i].size +"</div>";
+      text += "<div class='viewitem' >" + data[i].name + "<input type='hidden' name='" + data[i].size + "' value='" + data[i].place + "'></div>";
     }
 
     if(data.length % 3 == 1  && (data.length - i)<3 ){
-     text += "<div class='viewitem_n'></div>"
-     text += "<div class='viewitem_n'></div>"
-       }
-     if( i % 3 ==2){
+     text += "<div class='viewitem_n'></div>";
+     text += "<div class='viewitem_n'></div>";
+   }else if(data.length % 3 == 2  && (data.length - i)<2 ){
+     text += "<div class='viewitem_n'></div>";
+   }else if( i % 3 ==2){
        text += "</div>";
      }
   }
   $(".viewlist").html(text);
 }
 
+function cartpos(id,size,num){
 
-//----------------------------------
-
-$.get(baseurl + url,function(a){
-    cartUpdate(a);
-},"json");
-
-$(document).on('change','select',function(){
-    var $order_line = $(this).parent().parent();
-    var order_id = $order_line.children('.number').text();
-    var num = $order_line.children('.item_num').children('select').val();
-    var size = $order_line.children('.item_size').children('select').val();
-    editItem(order_id,num,size);
-});
-
-$('.cartin-btn').click(function(){
-        addItem();
-    });
-
-
-function addItem(){
-    var id = $(".item-selected").attr("id");
-    var size = "";
-        size = $();
-    var num = $();
-    var data = {
-        item_id : id,
-        "size" : size,
-        "num" : num
-    }
-    var url = 'api/order/add';
-    $.post(baseurl + url,data,function(a){
-        cartUpdate(a);
-    },"json");
-}
-
-function editItem(order_id,num,size){
-    var data = {
-        id : order_id,
-        num : num,
-        size : size,
-    }
-    var url = 'api/order/edit';
-    $.post(baseurl + url,data,function(a){
-        cartUpdate(a);
-    },"json");
-}
-
-function deleteItem(order_id){
-    var data = {
-        id : order_id
-    }
-    var url = 'api/order/delete';
-    $.post(baseurl + url,data,function(a){
-        cartUpdate(a);
-    },"json");
 }
 
 function cartUpdate(data){
-    //console.log(data);
-    var cart = document.getElementsByClassName('order_table_scroll')[0];
-    cart.innerHTML = "";
-    data['cart'].forEach(function(order,idx){
-        var element = document.createElement('tr');
-        var numHTML = "<select>";
-        for(var i = 1; i <= 5; i++){
-            var addStr = "<option value=" + i + ">" + i + "</option>"
-            if(order['num'] == i){
-                 addStr = "<option value=" + i + " selected>" + i + "</option>"
-            }
-            numHTML += addStr;
-        }
-        var sizeHTML = " ";
-        if(order['category'] == 'ピザ'){
-            sizeHTML = "<select>";
-            ['S','M','L'].forEach(function(size){
-                var addStr = "<option value=" + size.toLowerCase() + ">" + size + "</option>";
-                if(order['size'] == size.toLowerCase()){
-                     addStr = "<option value=" + size.toLowerCase() + " selected>" + size + "</option>";
-                }
-                sizeHTML += addStr;
-            });
-        }
+  var text = '';
 
-
-        element.innerHTML = "<td class=\"number\">" + (idx+1) + "</td><td class=\"item_name\">" + order['item_name'] + "</td><td class=\"item_num\">" + numHTML + "</td><td class=\"item_size\">"+ sizeHTML +"</td><td><input type=\"button\" class=\"cart-delete-btn\" value=\"削除\"></td></tr>";
-        cart.appendChild(element);
-    });
 }
